06297bf3b67fbd4d44394631490567844606a02d
Matt Brooke-Smith <matt@futureworkshops.com>
2016-01-02 13:47:50 +0800

    Update cloud_kit
    Add lookup

--- lib/cloud_kit.rb
+++ lib/cloud_kit.rb
@@ -34,15 +34,26 @@ class CloudKit
     response
   end
   
-  def query(record_type, record_name = nil, filter_by = nil, sort_by = nil, user_options = {})
+  def query(record_type, filter_by = nil, sort_by = nil, user_options = {})
     query_options = {:recordType => record_type}
-    query_options[:recordName] = record_name if record_name
     query_options[:filterBy] = filter_by if filter_by
     query_options[:sortBy] = sort_by if sort_by
     
     options = {:body => {:query => query_options}.to_json, :query => auth_options}
     options.merge! user_options
     self.class.post("#{uri_base}/records/query", options)    
+  end
+  
+  def lookup(record_names)
+    records = []
+    record_names.each do |record_name|
+      records << {:recordName => record_name}
+    end
+    query_options = {:records => records}
+    
+    options = {:body => {:query => query_options}.to_json, :query => auth_options}
+    options.merge! user_options
+    self.class.post("#{uri_base}/records/lookup", options)    
   end
   
   private 
